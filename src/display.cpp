12bd95ece029c43059dc069bbd5eed3fc5b2f48a
paudelkrishna1010 <paudelkrishna1010@gmail.com>
2026-01-26 22:36:11 +0530

    Implemented console size handling

--- src/display.cpp
+++ src/display.cpp
@@ -10,6 +10,22 @@
 #include <cpu_usage.h>
 #include <process_info.h>
 
+// Change function signature to accept a handle
+void getConsoleSize(HANDLE hActive, short *width, short *height)
+{
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    if (GetConsoleScreenBufferInfo(hActive, &csbi))
+    {
+        *width = csbi.srWindow.Right - csbi.srWindow.Left + 1;
+        *height = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
+    }
+    else
+    {
+        *width = 100;
+        *height = 25;
+    }
+}
+
 bool mainLoop()
 {
     FreeConsole();
@@ -18,13 +34,15 @@ bool mainLoop()
     short WIDTH = 100;
     short HEIGHT = 25;
     int scrollOffset = 0;
-    int visibleRows = HEIGHT - 1;
 
     HANDLE buffer1 = createConsoleBuffer();
     HANDLE buffer2 = createConsoleBuffer();
 
     HANDLE activeBuffer = buffer1;
     HANDLE backBuffer = buffer2;
+
+    getConsoleSize(activeBuffer, &WIDTH, &HEIGHT);
+    int visibleRows = HEIGHT - 1;
 
     CHAR_INFO *frameBuffer = createFrameBuffer(WIDTH, HEIGHT);
 
@@ -68,12 +86,53 @@ bool mainLoop()
         DWORD events;
         GetNumberOfConsoleInputEvents(handleInput, &events);
 
-        if (events > 0)
+        while (events > 0)
         {
             INPUT_RECORD record;
             DWORD read;
 
             ReadConsoleInput(handleInput, &record, 1, &read);
+            events--;
+
+            if (record.EventType == WINDOW_BUFFER_SIZE_EVENT)
+            {
+                getConsoleSize(activeBuffer, &WIDTH, &HEIGHT);
+                visibleRows = HEIGHT - 1;
+
+                if (frameBuffer)
+                    delete[] frameBuffer;
+                frameBuffer = createFrameBuffer(WIDTH, HEIGHT);
+
+                COORD newSize = {WIDTH, HEIGHT};
+                SMALL_RECT newWindow = {0, 0, WIDTH - 1, HEIGHT - 1};
+
+                auto SafeResize = [&](HANDLE hBuf)
+                {
+                    CONSOLE_SCREEN_BUFFER_INFO csbi;
+                    GetConsoleScreenBufferInfo(hBuf, &csbi);
+
+                    bool shrinking = (WIDTH < csbi.dwSize.X) || (HEIGHT < csbi.dwSize.Y);
+
+                    if (shrinking)
+                    {
+                        if (!SetConsoleWindowInfo(hBuf, TRUE, &newWindow))
+                        {
+                            SMALL_RECT minimal = {0, 0, 1, 1};
+                            SetConsoleWindowInfo(hBuf, TRUE, &minimal);
+                            SetConsoleWindowInfo(hBuf, TRUE, &newWindow);
+                        }
+                        SetConsoleScreenBufferSize(hBuf, newSize);
+                    }
+                    else
+                    {
+                        SetConsoleScreenBufferSize(hBuf, newSize);
+                        SetConsoleWindowInfo(hBuf, TRUE, &newWindow);
+                    }
+                };
+
+                SafeResize(buffer1);
+                SafeResize(buffer2);
+            }
 
             if (record.EventType == MOUSE_EVENT)
             {
@@ -87,6 +146,13 @@ bool mainLoop()
                         scrollOffset--;
                     else
                         scrollOffset++;
+
+                    int maxScroll = (int)processList.size() - visibleRows;
+                    if (maxScroll < 0)
+                        maxScroll = 0;
+
+                    if (scrollOffset > maxScroll)
+                        scrollOffset = maxScroll;
                 }
             }
         }
@@ -98,7 +164,7 @@ bool mainLoop()
         filterProcessArray(processIDArray, processCount);
 
         ULONGLONG now = GetTickCount64();
-        if (now - lastCpuUpdateTime > 500)
+        if (now - lastCpuUpdateTime > 1000)
         {
             updateCpu = true;
             getSystemTime(&currentSysTime);
@@ -135,8 +201,6 @@ bool mainLoop()
 
             processList.push_back(p);
             ProcessInfo &newProc = processList.back();
-            // strcpy(processList[processList.size() - 1].name, "[restricted]");
-            // fetchProcessName(processList.size() - 1);
 
             bool foundOld = false;
             for (auto &oldProc : oldList)
@@ -175,8 +239,12 @@ bool mainLoop()
 
         if (scrollOffset < 0)
             scrollOffset = 0;
-        if (scrollOffset > (int)processList.size() - visibleRows)
-            scrollOffset = processList.size() - visibleRows;
+        int maxScroll = (int)processList.size() - visibleRows;
+        if (maxScroll < 0)
+            maxScroll = 0;
+        if (scrollOffset > maxScroll)
+            scrollOffset = maxScroll;
+
         if (scrollOffset < 0)
             scrollOffset = 0;
 
@@ -189,10 +257,10 @@ bool mainLoop()
 
         clearFrameBuffer(frameBuffer, WIDTH, HEIGHT);
 
-        paintFrame(frameBuffer, 100, 0, 0, (char *)"PID");
-        paintFrame(frameBuffer, 100, 0, 10, (char *)"Name");
-        paintFrame(frameBuffer, 100, 0, 50, (char *)"Memory");
-        paintFrame(frameBuffer, 100, 0, 60, (char *)"CPU");
+        paintFrame(frameBuffer, WIDTH, 0, 0, (char *)"PID");
+        paintFrame(frameBuffer, WIDTH, 0, 10, (char *)"Name");
+        paintFrame(frameBuffer, WIDTH, 0, 50, (char *)"Memory");
+        paintFrame(frameBuffer, WIDTH, 0, 60, (char *)"CPU");
 
         for (int row = 0; row < visibleRows; row++)
         {
